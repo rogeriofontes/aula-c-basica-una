7fe788bd1d5dd540cfffe37f4b9e67b48fff6145
Payback <jvpetrocelli2004@gmail.com>
2024-11-18 02:40:43 -0300

    Update exercicio5.c

--- exercicio5.c
+++ exercicio5.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
+    char str[100];
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Iteração %d\n", i);
+    printf("Digite uma string: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    printf("String invertida: ");
+    for (int i = len - 1; i >= 0; i--) {
+        putchar(str[i]);
     }
+    printf("\n");
 
     return 0;
 }
