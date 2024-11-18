4eda791769439eeb5d11b48347cb7099b2264b0e
Payback <jvpetrocelli2004@gmail.com>
2024-11-18 02:41:34 -0300

    Update exercicio7.c

--- exercicio7.c
+++ exercicio7.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int arr[10], maior, menor;
 
-    int j = 0;
-    do
-    {
-        printf("j = %d\n", j);
-        j++;
-    } while (j < 5);
+    printf("Digite 10 números:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    maior = menor = arr[0];
+    for (int i = 1; i < 10; i++) {
+        if (arr[i] > maior)
+            maior = arr[i];
+        if (arr[i] < menor)
+            menor = arr[i];
+    }
+
+    printf("Maior elemento: %d\n", maior);
+    printf("Menor elemento: %d\n", menor);
 
     return 0;
 }
